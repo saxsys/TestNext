a1d0621c71c8c8eb72f5e17250969341ff074b21
PaulBrandesATSaxSys <paul.brandes@saxsys.de>
2017-10-04 14:36:58 +0200

    Fixed Testibility

---
+++ testNext.js
@@ -0,0 +1,12 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./src/SpecDeclaration/specDecorators/spec-decorators"));
+__export(require("./src/SpecDeclaration/assert/assert"));
+__export(require("./src/SpecDeclaration/assert/assertion-Error"));
+__export(require("./src/SpecDeclaration/assert/assert-proportion"));
+__export(require("./src/SpecStorage/specRegistry/spec-registry-storage"));
+__export(require("./src/testExec/TestNextRunning"));
+//# sourceMappingURL=testNext.js.map
